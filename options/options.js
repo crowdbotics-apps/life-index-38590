1c1855e3d83c6623d89841afa331323d9fcf4f8b
Crowdbotics <team@crowdbotics.com>
2023-01-31 03:41:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "life-index-38590",
+  url: "https://life-index-38590.botics.co",
+  api: "https://life-index-38590.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
